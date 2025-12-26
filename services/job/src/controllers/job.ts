13e8dc98c5511647d694a7ac0997a8130b00a60f
Anish <anishraj2601@gmail.com>
2025-12-26 17:33:40 +0530

    create and update job

--- services/job/src/controllers/job.ts
+++ services/job/src/controllers/job.ts
@@ -62,3 +62,129 @@ export const createCompany = TryCatch(
     });
   }
 );
+
+export const deleteCompany = TryCatch(
+  async (req: AuthenticatedRequest, res) => {
+    const user = req.user;
+
+    const { companyId } = req.params;
+
+    const [company] =
+      await sql`SELECT logo_public_id FROM companies WHERE company_id = ${companyId} AND recruiter_id = ${user?.user_id}`;
+
+    if (!company) {
+      throw new ErrorHandler(
+        404,
+        "Company not found or you're not authorized to delete it."
+      );
+    }
+
+    await sql`DELETE FROM companies WHERE company_id = ${companyId}`;
+
+    res.json({
+      message: "Company and all associated jobs have been deleted",
+    });
+  }
+);
+
+export const createJob = TryCatch(async (req: AuthenticatedRequest, res) => {
+  const user = req.user;
+
+  if (!user) {
+    throw new ErrorHandler(401, "Authentication required");
+  }
+
+  if (user.role !== "recruiter") {
+    throw new ErrorHandler(
+      403,
+      "Forbidden: Only recruiter can create a company"
+    );
+  }
+
+  const {
+    title,
+    description,
+    salary,
+    location,
+    role,
+    job_type,
+    work_location,
+    company_id,
+    openings,
+  } = req.body;
+
+  if (!title || !description || !salary || !location || !role || !openings) {
+    throw new ErrorHandler(400, "All the fields required");
+  }
+
+  const [company] =
+    await sql`SELECT company_id FROM companies WHERE company_id = ${company_id} AND recruiter_id = ${user.user_id}`;
+
+  if (!company) {
+    throw new ErrorHandler(404, "Company not found");
+  }
+
+  const [newJob] =
+    await sql`INSERT INTO jobs (title, description, salary, location, role, job_type, work_location, company_id, posted_by_recuriter_id, openings) VALUES (${title}, ${description}, ${salary}, ${location}, ${role}, ${job_type}, ${work_location}, ${company_id}, ${user.user_id}, ${openings}) RETURNING *`;
+
+  res.json({
+    message: "Job posted successfully",
+    job: newJob,
+  });
+});
+
+export const updateJob = TryCatch(async (req: AuthenticatedRequest, res) => {
+  const user = req.user;
+
+  if (!user) {
+    throw new ErrorHandler(401, "Authentication required");
+  }
+
+  if (user.role !== "recruiter") {
+    throw new ErrorHandler(
+      403,
+      "Forbidden: Only recruiter can create a company"
+    );
+  }
+
+  const {
+    title,
+    description,
+    salary,
+    location,
+    role,
+    job_type,
+    work_location,
+    company_id,
+    openings,
+    is_active,
+  } = req.body;
+
+  const [existingJob] =
+    await sql`SELECT posted_by_recuriter_id FROM jobs WHERE job_id = ${req.params.jobId}`;
+
+  if (!existingJob) {
+    throw new ErrorHandler(404, "Job not found");
+  }
+
+  if (existingJob.posted_by_recuriter_id !== user.user_id) {
+    throw new ErrorHandler(403, "Forbidden: You are not allowed");
+  }
+
+  const [updatedJob] = await sql`UPDATE jobs SET title = ${title},
+  description = ${description},
+  salary = ${salary},
+  location = ${location},
+  role = ${role},
+  job_type = ${job_type},
+  work_location = ${work_location},
+  openings = ${openings},
+  is_active = ${is_active}
+  WHERE job_id = ${req.params.jobId} RETURNING *;
+  `;
+
+  res.json({
+    message: "Job updated successfully",
+    job: updatedJob,
+  });
+});
