abb730c0c53a0f0dfaeece21eb6bf2fe6aa37b0e
Anish <anishraj2601@gmail.com>
2025-12-26 11:58:45 +0530

    auth commit

---
+++ services/auth/src/routes/auth.ts
@@ -0,0 +1,13 @@
+import express from "express";
+import { registerUser, loginUser, forgotPassword, resetPassword } from "../controllers/auth.js";
+import uploadFile from "../middleware/multer.js";
+
+const router = express.Router();
+
+router.post("/register",uploadFile, registerUser);
+router.post("/login", loginUser);
+router.post("/forgot", forgotPassword);
+router.post("/reset/:token", resetPassword);
+
+
+export default router;
