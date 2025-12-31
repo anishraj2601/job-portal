085f21a73accd23ed172a826df4a37ad877386ac
Anish <anishraj2601@gmail.com>
2025-12-31 10:26:20 +0530

    get and update application for recruiter

--- services/job/src/controllers/job.ts
+++ services/job/src/controllers/job.ts
@@ -4,6 +4,8 @@ import getBuffer from "../utils/buffer.js";
 import { sql } from "../utils/db.js";
 import ErrorHandler from "../utils/errorHandler.js";
 import { TryCatch } from "../utils/TryCatch.js";
+import { applicationStatusUpdateTemplate } from "../template.js";
+import { publishToTopic } from "../producer.js";
 
 export const createCompany = TryCatch(
   async (req: AuthenticatedRequest, res) => {
@@ -259,3 +261,89 @@ export const getSingleJob = TryCatch(async (req, res) => {
 
   res.json(job);
 });
+
+export const getAllApplicationForJob = TryCatch(
+  async (req: AuthenticatedRequest, res) => {
+    const user = req.user;
+
+    if (!user) {
+      throw new ErrorHandler(401, "Authentication required");
+    }
+
+    if (user.role !== "recruiter") {
+      throw new ErrorHandler(403, "Forbidden: Only recruiter can access this");
+    }
+
+    const { jobId } = req.params;
+
+    const [job] = await sql`
+    SELECT posted_by_recuriter_id FROM jobs WHERE job_id = ${jobId}
+    `;
+
+    if (!job) {
+      throw new ErrorHandler(404, "job not found");
+    }
+
+    if (job.posted_by_recuriter_id !== user.user_id) {
+      throw new ErrorHandler(403, "Forbidden you are not allowed");
+    }
+
+    const applications =
+      await sql`SELECT * FROM applications WHERE job_id = ${jobId} ORDER BY subscribed DESC, applied_at ASC`;
+
+    res.json(applications);
+  }
+);
+
+export const updateApplication = TryCatch(
+  async (req: AuthenticatedRequest, res) => {
+    const user = req.user;
+
+    if (!user) {
+      throw new ErrorHandler(401, "Authentication required");
+    }
+
+    if (user.role !== "recruiter") {
+      throw new ErrorHandler(403, "Forbidden: Only recruiter can access this");
+    }
+
+    const { id } = req.params;
+
+    const [application] =
+      await sql`SELECT * FROM applications WHERE application_id = ${id}`;
+
+    if (!application) {
+      throw new ErrorHandler(404, "Application not found");
+    }
+
+    const [job] =
+      await sql`SELECT posted_by_recuriter_id, title FROM jobs WHERE job_id = ${application.job_id}`;
+
+    if (!job) {
+      throw new ErrorHandler(404, "no job with this id");
+    }
+
+    if (job.posted_by_recuriter_id !== user.user_id) {
+      throw new ErrorHandler(403, "Forbidden! you are not allowed");
+    }
+
+    const [updatedApplication] =
+      await sql`UPDATE applications SET status = ${req.body.status} WHERE application_id = ${id} RETURNING *`;
+
+    const message = {
+      to: application.applicant_email,
+      subject: "Application Update - Job portal",
+      html: applicationStatusUpdateTemplate(job.title),
+    };
+
+    publishToTopic("send-mail", message).catch((error) => {
+      console.error("Failed to publish message to kafka", error);
+    });
+
+    res.json({
+      message: "Application updated",
+      job,
+      updatedApplication,
+    });
+  }
+);
--- services/job/src/routes/job.ts
+++ services/job/src/routes/job.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import { isAuth } from "../middlewares/auth.js";
 import uploadFile from "../middlewares/multer.js";
-import { createCompany, createJob, deleteCompany, getAllActiveJobs, getAllCompany, getCompanyDetails, getSingleJob, updateJob } from "../controllers/job.js";
+import { createCompany, createJob, deleteCompany, getAllActiveJobs, getAllApplicationForJob, getAllCompany, getCompanyDetails, getSingleJob, updateApplication, updateJob } from "../controllers/job.js";
 
 const router = express.Router();
 
@@ -13,5 +13,7 @@ router.get("/company/all", isAuth, getAllCompany);
 router.get("/company/:id", getCompanyDetails);
 router.get("/all", getAllActiveJobs);
 router.get("/:jobId", getSingleJob);
+router.get("/application/:jobId", isAuth, getAllApplicationForJob);
+router.put("/application/update/:id", isAuth, updateApplication);
 
 export default router;
