64509c1df1f5eb516dc82619a5e3d06d3e50fcc3
Anish <anishraj2601@gmail.com>
2026-03-13 08:08:59 +0530

    frontend init code

---
+++ client/src/app/page.tsx
@@ -0,0 +1,14 @@
+import CareerGuide from "@/components/career-guide";
+import Hero from "@/components/hero";
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <Hero/>
+      <CareerGuide/>
+    </div>
+  );
+};
+
+export default Home;
---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+const Home = () => {
+  return ( 
+  <div>
+  <Button variant={"secondary"}>Shadcn button</Button>
+  </div> );
+}
+ 
+export default Home;
